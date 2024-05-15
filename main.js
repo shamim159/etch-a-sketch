835a45efd7fa79500b48f59580b965897f60340d
shamim159 <shamin_ali@hotmail.co.uk>
2024-05-15 12:55:29 +0100

    grid created using for loop

---
+++ main.js
@@ -0,0 +1,11 @@
+//main div container
+const container = document.querySelector("#container");
+
+
+//little divs
+for (let i = 0; i <272; i++) {// added extra as grid was uneven
+  const div = document.createElement("div");
+  container.appendChild(div);
+}
+
+
