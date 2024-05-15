bf4042020a0a152113f44d0cf5d99fdb2ca43e78
shamim159 <shamin_ali@hotmail.co.uk>
2024-05-15 13:41:09 +0100

    added mouse enter function

--- main.js
+++ main.js
@@ -3,9 +3,15 @@ const container = document.querySelector("#container");
 
 
 //little divs
+
+
 for (let i = 0; i <272; i++) {// added extra as grid was uneven
   const div = document.createElement("div");
   container.appendChild(div);
+  
+  div.addEventListener("mouseenter", mouseEnter);
+  
+  function mouseEnter(){
+    div.style.backgroundColor = "red";
+  }
 }
-
-
